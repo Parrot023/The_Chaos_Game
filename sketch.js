c02e7dd10da7e116e20b4de57c0ddcd062cb6c6f
Parrot023 <Andreas081203@gmail.com>
2018-11-30 21:40:35 +0100

    added comments + option to modify number of dots and fraction

--- sketch.js
+++ sketch.js
@@ -1,69 +1,110 @@
 //Chaos game by Parrot
 //the triangle is called the the sierpinski triangle
+//wikipedia
+//https://en.wikipedia.org/wiki/Chaos_game
+//the coding train (YouTube)
+//https://www.youtube.com/watch?v=7gNzMtYo9n4
+
+//array of points/dots
+var points = [];
+//background color
+var backgroundColor = 255;
+//dot/point color
+var dotColor = 0;
+//fraction to move
+var fraction = 0.5;
+
+var number_of_dots = 3;
 
 function setup() {
-  createCanvas(600, 600);
-  background(0);
-  a = createVector(random(width), random(height));
-  b = createVector(random(width), random(height));
-  c = createVector(random(width), random(height));
-  rp = createVector(random(width), random(height));
+  //initialyse canvas
+  createCanvas(400, 400);
+  background(backgroundColor);
 
-  stroke(255,0,255);
+  //create start dots/points
+  // for (var i=0; i<number_of_dots; i++) {
+  //   a = createVector(random(width), random(height));
+  //   points.push(a);
+  // }
+
+  //creates start dots around circle
+  for (let i = 0; i <number_of_dots; i++) {
+   let angle = i * TWO_PI / number_of_dots;
+   let a = p5.Vector.fromAngle(angle);
+   a.mult(width / 2);
+   a.add(width / 2, height / 2);
+   points.push(a);
+  }
+
+  //draw start dots
+  stroke(dotColor);
   strokeWeight(5);
-  point(a.x,a.y);
-  point(b.x,b.y);
-  point(c.x,c.y);
+  for (var i=0; i<number_of_dots; i++) {
+    point(points[i].x, points[i].y);
+  }
+
+  //vertex point/dot
+  vertex = createVector(random(width), random(height));
 
 }
 
+//reset drawing/canvas
 function reset() {
-  background(0);
-  a = createVector(random(width), random(height));
-  b = createVector(random(width), random(height));
-  c = createVector(random(width), random(height));
-  rp = createVector(random(width), random(height));
-	stroke(255,0,255);
+
+  background(backgroundColor);
+
+  //create start dots/points
+  // points = [];
+  // for (var i=0; i<number_of_dots; i++) {
+  //   a = createVector(random(width), random(height));
+  //   points.push(a);
+  // }
+
+  //creates start dots around circle
+  for (let i = 0; i <number_of_dots; i++) {
+   let angle = i * TWO_PI / number_of_dots;
+   let a = p5.Vector.fromAngle(angle);
+   a.mult(width / 2);
+   a.add(width / 2, height / 2);
+   points.push(a);
+  }
+
+  //draw start points/dots
+  stroke(dotColor);
   strokeWeight(5);
-  point(a.x,a.y);
-  point(b.x,b.y);
-  point(c.x,c.y);
-}
+  for (var i=0; i<number_of_dots; i++) {
+    point(points[i].x ,points[i].y);
+  }
+
+
+  //vertex point
+  vertex = createVector(random(width), random(height));
+
+
+}//end of reset
 
 function draw() {
 
-  stroke(255);
+  stroke(dotColor);
   strokeWeight(1);
-  point(rp.x, rp.y);
 
-	for (let i = 0; i < 100; i++) {
-    let r = floor(random(3));
-
-    if (r == 0) {
-      rp.x = lerp(rp.x, a.x, 0.5);
-      rp.y = lerp(rp.y, a.y, 0.5);
-      stroke(0,0,255);
-      point(rp.x, rp.y);
-    }
-
-    if (r == 1) {
-      rp.x = lerp(rp.x, b.x, 0.5);
-      rp.y = lerp(rp.y, b.y, 0.5);
-      stroke(0,255,0);
-      point(rp.x, rp.y);
-    }
-
-    if (r == 2) {
-      rp.x = lerp(rp.x, c.x, 0.5);
-      rp.y = lerp(rp.y, c.y, 0.5);
-      stroke(255,0,0);
-      point(rp.x, rp.y);
-    }
-
-
+  //draws dots
+  for (let o = 0; o < 100; o++) {
+    for (var i=0; i<number_of_dots; i++) {
+      //chooses a random of the 3 or more start points
+      let r = random(points);
+      //calculates the next vertex by moving a fraction of
+      //the distance between previus vertex and the random chosen start point
+      vertex.x = lerp(vertex.x, r.x, fraction);
+      vertex.y = lerp(vertex.y, r.y, fraction);
+      //draw vertex
+      stroke(dotColor);
+      point(vertex.x, vertex.y);
+    }//end of for loop
   }//end of for loop
 
-  if (frameCount % 100 == 0) {
+  //resets every 100 frames
+  if (frameCount % 200 == 0) {
    reset();
   }
 }//end of draw
